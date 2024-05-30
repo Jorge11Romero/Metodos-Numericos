e0d512402b427b8bace1616f9ff4a910e63ce47a
Jorge de Jesús Romero López <147437900+Jorge11Romero@users.noreply.github.com>
2024-05-30 01:43:29 -0600

    Update Implementación.java

--- TEMA_2/Método de Newton-Raphson/Implementación.java
+++ TEMA_2/Método de Newton-Raphson/Implementación.java
@@ -1,28 +1,30 @@
-public class Biseccion {
+public class NewtonRaphson {
     
     public static double funcion(double x) {
         return x * x - 4;
     }
-
-    public static double biseccion(double a, double b, double tolerancia) {
-        double c = a;
-        while ((b - a) >= tolerancia) {
-            c = (a + b) / 2;
-            
-            if (funcion(c) == 0.0) {
-                break;
-            } else if (funcion(c) * funcion(a) < 0) {
-                b = c;
-            } else {
-                a = c;
-            }
-        }
-        return c;
+    
+    public static double derivada(double x) {
+        return 2 * x;
     }
-
+    
+    public static double newtonRaphson(double x0, double tolerancia) {
+        double x = x0;
+        double h = funcion(x) / derivada(x);
+        
+        while (Math.abs(h) >= tolerancia) {
+            h = funcion(x) / derivada(x);
+            x = x - h;
+        }
+        
+        return x;
+    }
+    
     public static void main(String[] args) {
-        double a = 0, b = 3, tolerancia = 0.0001;
-        double raiz = biseccion(a, b, tolerancia);
-        System.out.println("La raíz es: " + raiz);
+        double x0 = 2; 
+        double tolerancia = 0.0001;
+        double raiz = newtonRaphson(x0, tolerancia);
+        
+        System.out.printf("La raíz es: %.4f%n", raiz);
     }
 }
