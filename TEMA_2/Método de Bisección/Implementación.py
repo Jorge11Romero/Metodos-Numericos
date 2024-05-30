a81caa28e38aee6e0a376f3147c9200a09177a74
Jorge de Jesús Romero López <147437900+Jorge11Romero@users.noreply.github.com>
2024-05-30 01:14:12 -0600

    Update Implementación.py

--- TEMA_2/Método de Bisección/Implementación.py
+++ TEMA_2/Método de Bisección/Implementación.py
@@ -1,25 +1,45 @@
-def lagrange(x, y, xi):
-    resultado = 0.0
-    n = len(x)
+# Algoritmo de Bisección
 
-    for i in range(n):
-        termino = y[i]
-        for j in range(n):
-            if j != i:
-                termino *= (xi - x[j]) / (x[i] - x[j])
-        resultado += termino
+from os import error
+import numpy as np
 
-    return resultado
+# INGRESO
+fx = lambda x: x**3 + 4*x**2 - 10 
+xI = 1
+xS = 2
+error = .005
 
-def main():
-    # Puntos de datos (x, y)
-    x = [0, 1, 2, 3]
-    y = [1, 3, 2, 5]
+intervalo = xS-xI
 
-    xi = 2.5
+fxI = fx(xI)
+fxS = fx(xS)
+i = 1
+while (intervalo>error):
+    xr = (xI+xS)/2
+    fxr = fx(xr)
+   
+    i = i + 1
+                 
+    cambia = np.sign(fxI)*np.sign(fxr)
+    if (cambia<0):
+        xS = xr
+        fxS = fxr
+    else:
+        xI=xr
+        fxI = fxr
+    intervalo = xS-xI
+xr = (xI+xS)/2
+fxr = fx(xr)
 
-    yi = lagrange(x, y, xi)
-    print("Valor interpolado en x =", xi, "es y =", yi)
 
-if __name__ == "__main__":
-    main()
+raiz = xr
+
+
+np.set_printoptions(precision = 4)
+
+
+print("La raiz es: ",raiz)
+
+#Quiero establecer a cuatro dígitos
+print()
+print ("La raiz en cuatro numeros despues del punto: ", "{0:.4f}".format(raiz))
